659661924ac04960b9bbc683c74abd85902543e2
wangchx67 <99788482+wangchx67@users.noreply.github.com>
2026-03-08 13:21:26 +0000

    Deploying to gh-pages from @ wangchx67/wangchx67.github.io@9e5b2a69771e64453840e040d0e15f5862f2e1bf 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,7 +30,29 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "post-flux2-0-technique-report-and-code",
+        },{id: "post-dynamic-region-aware-convolution-drconv",
+        
+          title: "Dynamic Region-Aware Convolution (DRConv)",
+        
+        description: "旷世CVPR2021论文Dynamic Region-Aware Convolution介绍及其在底层视觉中的应用",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2026/dynamic-region-aware-convolution/";
+          
+        },
+      },{id: "post-qwen-image-technique-report-and-code",
+        
+          title: "qwen-image technique report and code",
+        
+        description: "学习下qwen-image",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2026/qwen-image/";
+          
+        },
+      },{id: "post-flux2-0-technique-report-and-code",
         
           title: "flux2.0 technique report and code",
         
